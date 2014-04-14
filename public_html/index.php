f10add9c3c93ae553876cdbdf58fcdc286594e90
Ahmad Milzam <ahmadmilzam@yahoo.com>
2014-04-14 10:44:37 +0700

    change directory system

--- public_html/index.php
+++ public_html/index.php
@@ -75,7 +75,7 @@ if (defined('ENVIRONMENT'))
  * as this file.
  *
  */
-	$system_path = '../system';
+	$system_path = '../ci/system';
 
 /*
  *---------------------------------------------------------------
@@ -91,7 +91,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = '../application';
+	$application_folder = '../ci/application';
 
 /*
  * --------------------------------------------------------------------
